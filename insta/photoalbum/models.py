e3a9a6659c2bca7be753ecc8f4ce66151aa225de
agatagmaj <agatagmaj@gmail.com>
2018-09-28 13:43:33 +0200

    Photo model

--- insta/photoalbum/models.py
+++ insta/photoalbum/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Photo(models.Model):
+    path = models.CharField(max_length=256)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
